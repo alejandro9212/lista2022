7d4a5a62de72782c0984f29486cbaca6a51584d3
jhon tobon <ian.stobon2018@gmail.com>
2022-06-07 17:47:42 -0500

    creacion de javaScript

---
+++ js/app.js
@@ -0,0 +1,29 @@
+let count=0;
+const input=document.querySelector('input[type="text"]');
+
+
+dtform.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    addlisck();
+})
+
+let addlisck=()=>{
+    count++;
+    newtext=input.value;
+    list.innerHTML +=
+    `<div class="col-12 md-12 lg-12 task_container" id="${count}">
+       <label for="">
+         <input type="checkbox">
+           ${newtext}
+         </label>
+    <img src="img/iconos/delete.png" class="closebtn">
+ </div>`
+ input.value="";
+ updatechek();
+}
+
+let updatechek=()=>{
+    let pend=list.querySelectorAll('input');
+    let comp=list.querySelectorAll('input[type="checkbox"]:checked');
+    stars.innerHTML+=`<p>tareas pendientes : ${pend.length} completadas: ${comp}</p>`
+}
